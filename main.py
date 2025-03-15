a4797356fa8b2b29985615223ad4912261567c53
Ryan-Webb62 <rmagnus62@gmail.com>
2025-03-15 10:09:26 -0400

    added clock and circle obj

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import pygame
 from constants import *
 
 def main():
-  print(f"""Starting Asteroids!\n
-Screen width: {SCREEN_WIDTH}\n
-Screen height: {SCREEN_HEIGHT}\n""")
-
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
   
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
       
-    screen.fill((0, 0, 0))
+    screen.fill(("black"))
     pygame.display.flip()
-
+    
+    # limit the frame rate to 60 frames per second
+    dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
